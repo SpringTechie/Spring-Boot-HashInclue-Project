9d8e1a4e9893bf16926b90ef5f63407d06edf72a
springtechie <springframeworkdeveloper@gmail.com>
2025-09-17 18:45:52 +0530

    This is initial commit to hashinclude project

---
+++ src/main/java/com/hashinclude/SpringBootHashInclueProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hashinclude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHashInclueProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHashInclueProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hashinclude/SpringBootHashInclueProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashinclude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHashInclueProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
