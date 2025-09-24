b2774c796a2c7a5f2e3ff871830f91da915705f5
springtechie <springframeworkdeveloper@gmail.com>
2025-09-24 09:53:38 +0530

    Addded Mysql connection

--- src/main/java/com/hashinclude/models/User.java
+++ src/main/java/com/hashinclude/models/User.java
@@ -1,12 +1,37 @@
 package com.hashinclude.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
 
+    @Id
     public int id;
     public String name;
 
     public User(int id, String name) {
         this.id = id;
+        this.name = name;
+    }
+
+    public User() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
