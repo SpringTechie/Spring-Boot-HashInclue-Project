1e5644199a96006e8a37d5ff5dfa79dc8ec3d94e
springtechie <springframeworkdeveloper@gmail.com>
2025-10-06 09:52:11 +0530

    Added profile code

--- src/main/java/com/hashinclude/config/CustomBeans.java
+++ src/main/java/com/hashinclude/config/CustomBeans.java
@@ -3,8 +3,10 @@ package com.hashinclude.config;
 import com.hashinclude.services.ClassA;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile(value = {"dev","stage"})
 public class CustomBeans {
 
     public CustomBeans() {
