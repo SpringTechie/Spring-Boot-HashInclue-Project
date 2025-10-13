d5c093d60627c46baf29fa3178a47ea873fdfa4d
springtechie <springframeworkdeveloper@gmail.com>
2025-10-13 09:43:28 +0530

    Added interceptors

---
+++ src/main/java/com/hashinclude/filters/RequestInterceptor.java
@@ -0,0 +1,32 @@
+package com.hashinclude.filters;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class RequestInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if(request.getRequestURI().equalsIgnoreCase("/error")) {
+            return false;
+        }
+        System.out.println("PreHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("Hello PostHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        System.out.println("After completion");
+    }
+}
